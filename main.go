d190155164c8e3b178325cfad428d794327794a4
Martin Kraft <martin@upspin.org>
2023-05-06 06:14:35 -0400

    Updates comments.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"example.com/entexample/ent"
@@ -11,30 +12,31 @@ import (
 func main() {
 	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
 	if err != nil {
-		log.Fatalf("failed opening connection to sqlite: %v", err)
+		log.Printf("failed opening connection to sqlite: %v", err)
 	}
 	defer client.Close()
 	// Run the auto migration tool.
 	if err := client.Schema.Create(context.Background()); err != nil {
-		log.Fatalf("failed creating schema resources: %v", err)
+		log.Printf("failed creating schema resources: %v", err)
 	}
 
 	user := client.User.Create().SetType("admin").SaveX(context.Background())
 
-	user, err = client.User.Get(context.Background(), user.ID)
+	_, err = client.User.Get(context.Background(), user.ID)
 	if err != nil {
-		log.Fatalf("failed getting user: %v", err)
+		log.Printf("failed getting user: %v", err)
 	}
-	log.Printf("after create\t%v\t%s", user, "proving that it was saved correctly")
+	user = client.User.GetX(context.Background(), user.ID)
+	fmt.Printf("after Create\t%v\t%s\n", user, "saved correctly")
 
 	user.Type = nil
-	log.Printf("before update\t%v\t\t%s", user, "note that type is nil")
+	fmt.Printf("before Update\t%v\t\t%s\n", user, "type is nil")
 
 	user.Update().SaveX(context.Background())
 	user = client.User.GetX(context.Background(), user.ID)
-	log.Printf("after update\t%v\t%s", user, "the nil value is not saved by (*ent.User).Update")
+	fmt.Printf("after Update\t%v\t%s\n", user, "nil value is not saved by Update alone")
 
 	user.Update().ClearType().SaveX(context.Background())
 	user = client.User.GetX(context.Background(), user.ID)
-	log.Printf("after cleartype\t%v\t\t%s", user, "now type is actually nil")
+	fmt.Printf("after ClearType\t%v\t\t%s\n", user, "nil value is saved by ClearType")
 }
